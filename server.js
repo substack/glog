833015e8650c866df019538bbfb364b469153711
James Halliday <mail@substack.net>
2012-12-23 18:31:20 -0800

    httpsec doesn't even work with libcurl

--- server.js
+++ server.js
@@ -20,20 +20,8 @@ var opts = {
 };
 
 var server = https.createServer(opts, function (req, res) {
-console.dir(req.connection.pair.credentials);
     if (/[^\?]+\.git/.test(req.url)) {
-        res.statusCode = 401;
-        res.setHeader('www-authenticate', [
-            'httpsec/1.0 initialize',
-            'id=localhost:5000',
-            'dh=+NcclW9y2I3W9X5Vy+5v5lAy4X56y+Ncrwrtv5lqe',
-            'certificate=http://localhost:5000/cert.pem',
-            'token=mCa5tx1vKBY',
-            'auth=vpCNmx7MZ7iqgkzIe0HWwfyrOMeqwg0TdbpwefI',
-            'signature=2pX3SNgzWkV3w0W9y2X5V23hhy+5b8DQmo'
-        ].join('\n    '));
-        res.end();
-        //repos.handle(req, res);
+        repos.handle(req, res);
     }
     else ecstatic(req, res);
 });
