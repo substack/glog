d225a5f7b674a020a6f81a041bb201f22e7c5eb4
James Halliday <mail@substack.net>
2012-12-23 20:30:40 -0800

    just http for now

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var https = require('https');
+var http = require('http');
 var pushover = require('pushover');
 var mkdirp = require('mkdirp');
 
@@ -12,38 +12,11 @@ repos.on('push', function (push) {
 });
 
 var fs = require('fs');
-var opts = {
-    cert : fs.readFileSync(__dirname + '/server-keys/cert.pem'),
-    key : fs.readFileSync(__dirname + '/server-keys/key.pem'),
-    ca : [ fs.readFileSync(__dirname + '/server-keys/csr.pem') ],
-    requestCert : true
-};
 
-var server = https.createServer(opts, function (req, res) {
+var server = http.createServer(opts, function (req, res) {
     if (/[^\?]+\.git/.test(req.url)) {
-        var auth = req.headers.authorization;
-        var m = /^Basic (\S+)/i.exec(auth);
-        if (!auth || !m) return authenticate();
-        
-        var s = Buffer(m[1], 'base64').toString().split(':');
-        var user = s[0], pass = s[1];
-        if (pass === '') return authenticate();
-        
-        if (user !== 'substack' || pass !== 'pow') {
-            res.statusCode = 403;
-            res.setHeader('www-authenticate', 'basic');
-            res.end('not authorized');
-            return;
-        }
-        console.dir([ user, pass ]);
-        //repos.handle(req, res);
+        repos.handle(req, res);
     }
     else ecstatic(req, res);
-    
-    function authenticate () {
-        res.statusCode = 401;
-        res.setHeader('www-authenticate', 'basic');
-        res.end('authenticate');
-    }
 });
 server.listen(5000);
